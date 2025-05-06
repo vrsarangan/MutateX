85ade66c173e534794f527ee8d827de1d267836f
vrsarangan <111537537+vrsarangan@users.noreply.github.com>
2025-05-06 10:17:53 +0530

    Delete ui directory

--- ui/scan.py
+++
@@ -1,9 +0,0 @@
-# ui/scan.py
-
-import tkinter as tk
-
-def create_scan(content_frame):
-    frame = tk.Frame(content_frame, bg="#f0f0f0")
-    label = tk.Label(frame, text="🧪 Start a New Scan", font=("Arial", 24), bg="#f0f0f0")
-    label.pack(pady=100)
-    return frame
